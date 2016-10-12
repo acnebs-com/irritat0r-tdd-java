57f9f94edc9a00102800957f8bebf66cb9d4160c
Andreas Czakaj <andreas.czakaj@mensemedia.net>
2016-10-12 23:50:07 +0200

    add Irritat0rMessagePool

---
+++ src/main/java/com/acnebs/posts/irritat0r/Irritat0rMessagePool.java
@@ -0,0 +1,10 @@
+package com.acnebs.posts.irritat0r;
+/**
+ * Class Irritat0rMessagePool.
+ * <p>
+ * Created by andreas.czakaj on 12.10.2016
+ *
+ * @author andreas.czakaj
+ */
+class Irritat0rMessagePool {
+}
---
+++ src/test/java/com/acnebs/posts/irritat0r/Irritat0rMessagePoolTest.java
@@ -0,0 +1,10 @@
+package com.acnebs.posts.irritat0r;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Irritat0rMessagePoolTest {
+    @Test
+    public void test_new() throws Exception {
+        assertNotNull(new Irritat0rMessagePool());
+    }
+}
