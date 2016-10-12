79b712065cbe8716602827ae74d935cb1ff3fd93
Andreas Czakaj <andreas.czakaj@mensemedia.net>
2016-10-13 00:51:16 +0200

    add Irritat0rMessagePool

--- src/main/java/com/acnebs/posts/irritat0r/Irritat0rService.java
+++ src/main/java/com/acnebs/posts/irritat0r/Irritat0rService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 class Irritat0rService {
     private final Irritat0rMessagePool messagePool;
     private static final String TEMPLATE = "Hey %s, did you know that %s?";
+    private static final String FALLBACK_SALUTATION = "you";
 
     public Irritat0rService(final Irritat0rMessagePool messagePool) {
         this.messagePool = messagePool;
@@ -20,12 +21,11 @@ class Irritat0rService {
     }
 
     String convertSalutation(Optional<String> maybeSalutation) {
-        final String fallback = "you";
-
-        String salutation = maybeSalutation.orElse(fallback);
+        String salutation = maybeSalutation.orElse(FALLBACK_SALUTATION);
         salutation = salutation.trim();
+
         if (salutation.length() < 1) {
-            salutation = fallback;
+            salutation = FALLBACK_SALUTATION;
         }
 
         return salutation;
